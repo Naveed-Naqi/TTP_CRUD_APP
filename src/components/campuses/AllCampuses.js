26ab77de832e5b9686c4dffb8e9bcac790db7b5b
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-07 10:59:54 -0500

    added basic homepage and established filestructure

---
+++ src/components/campuses/AllCampuses.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Campuses extends Component {
+    render() {
+        return (
+            <div>
+                <h1>All Campuses</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/students/AllStudents.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Students extends Component {
+    render() {
+        return (
+            <div>
+                <h1>All Students</h1>
+            </div>
+        )
+    }
+}
